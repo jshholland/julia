891f6f7abaf6e0ecfb05de9c6b1fa7efaf73345a
Josh Holland <jrh@joshh.co.uk>
2014-05-16 18:18:43 +0100

    Add some kind of command line interface

--- julia.go
+++ julia.go
@@ -6,16 +6,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image/png"
 	"os"
 )
 
-func main() {
-	f := NewPoly(-1+.1i, 0, 1)
-	m := Draw(f, -2, 2, -2, 2, 0.001)
+var (
+	re   = flag.Float64("real", 0.0, "real part of the constant whose quadratic Julia set to plot")
+	im   = flag.Float64("imag", 0.0, "imaginary part of the constant whose quadratic Julia set to plot")
+	size = flag.Int("size", 4000, "size in pixels to draw fractal at")
+	out  = flag.String("out", "julia.png", "filename to save output to")
+)
 
-	out, err := os.Create("julia.png")
+func main() {
+	flag.Parse()
+	f := NewPoly(complex(*re, *im), 0, 1)
+	m := Draw(f, -2, 2, -2, 2, 4/float64(*size))
+
+	out, err := os.Create(*out)
 	if err != nil {
 		fmt.Println(err)
 		return
